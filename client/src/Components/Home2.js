bc10a658748226b94b694d6ba2ad7ee21d7fe121
bella3212 <106274360+bella3212@users.noreply.github.com>
2023-01-04 15:51:07 +0200

    Update Home2.js

--- client/src/Components/Home2.js
+++ client/src/Components/Home2.js
@@ -10,6 +10,8 @@ import Title from "./Title"
 import add_dynamic_components from '../utils/makeData'
 import { Add_form_for_search } from '../utils/generic2'
 import { Link } from 'react-router-dom';
+import Clock from "./Clock"
+
 const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
@@ -97,6 +99,7 @@ export function Home2() {
           })}
         >
           <div className={classes.drawerHeader} />
+          <Clock txt="Home Time"></Clock>
           <Title message={process.env.REACT_APP_TITLE_MESSAGE} />
           {/* <form onSubmit={handleSubmit}>
             <input type="text" />
